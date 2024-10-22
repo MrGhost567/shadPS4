9d758dbd8eab7f80f6de9cf872f932210c3f39f3
Frodo Baggins <baggins31084@proton.me>
2024-10-21 22:27:41 -0700

    fix SSA pass. Keep Identity (still useful). Split into 2 ReplaceUses variants

--- src/shader_recompiler/recompiler.cpp
+++ src/shader_recompiler/recompiler.cpp
@@ -83,6 +83,7 @@ IR::Program TranslateProgram(std::span<const u32> code, Pools& pools, Info& info
     // Run optimization passes
     dumpMatchingIR("pre_ssa");
     Shader::Optimization::SsaRewritePass(program.post_order_blocks);
+    Shader::Optimization::IdentityRemovalPass(program.blocks);
     dumpMatchingIR("pre_const_prop");
     Shader::Optimization::ConstantPropagationPass(program.post_order_blocks);
     if (program.info.stage != Stage::Compute) {
